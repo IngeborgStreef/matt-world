992eb906656fcf8ff6af198cad55b1e6a7536ae1
IngeborgStreef <75255444+IngeborgStreef@users.noreply.github.com>
2021-06-15 12:20:29 +0200

    initial commit

---
+++ src/test/java/nl/mattworld/MattBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mattworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
