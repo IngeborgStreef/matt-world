6a363648853369f266de80d0df9a11424255b7f0
IngeborgStreef <75255444+IngeborgStreef@users.noreply.github.com>
2021-06-23 09:08:11 +0200

    made first mockMvc test for BookControllerTest. Time to finish the rest

---
+++ src/test/java/nl/mattworld/book/BookControllerTest.java
@@ -0,0 +1,37 @@
+package nl.mattworld.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = BookController.class)
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void getAllBooks() throws Exception {
+        when(bookService.retrieveBooks()).thenReturn(List.of(new Book(), new Book()));
+        this.mockMvc.perform(get("/api/books")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    public void createBook() {
+
+    }
+}
